eab42b857c24f5ea4c2f16787fbe01534490c623
Amera <amera.boudia84@gmail.com>
2022-08-26 16:15:40 +0200

    first commit

---
+++ back/api.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+# instantiate app
+app = Flask(__name__)
+api = Api(app)
+
+class Product(Resource):
+  def get(self):
+     return { 'products' : ['Product 1','Product 2','Product 3','Product 4','Product 5','Product 6','Product 7']}
+
+
+
+#create routes 
+api.add_resource(Product,'/')
+
+#run the app
+
+if  __name__ == '__main__':
+   app.run(host='0.0.0.0',port=80,debug=True)
+
